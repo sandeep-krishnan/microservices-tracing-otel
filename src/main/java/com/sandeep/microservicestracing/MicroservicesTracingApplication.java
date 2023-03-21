bc83e0638c1304414c308cd4a0b9c76f5ba843d3
Sandeep Krishnan <krsandeep@athenahealth.com>
2023-03-21 15:40:25 +0530

    initial commit

---
+++ src/main/java/com/sandeep/microservicestracing/MicroservicesTracingApplication.java
@@ -0,0 +1,13 @@
+package com.sandeep.microservicestracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesTracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesTracingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandeep/microservicestracing/MicroservicesTracingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandeep.microservicestracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesTracingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
